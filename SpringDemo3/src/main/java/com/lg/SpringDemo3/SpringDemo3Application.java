840edc03856259ec79fd0290b10a2b5f53017b07
gangsijay <8gangsi@163.com>
2018-07-04 15:03:00 +0800

    23

---
+++ SpringDemo3/src/main/java/com/lg/SpringDemo3/SpringDemo3Application.java
@@ -0,0 +1,12 @@
+package com.lg.SpringDemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo3Application.class, args);
+	}
+}
